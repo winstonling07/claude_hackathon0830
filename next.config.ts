a86c4b52d1723357bb2070681d899a44384a1383
dchu0307 <danielchu319@gmail.com>
2025-11-14 17:11:35 -0800

    removing nextjs dev indicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  devIndicators: false
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
